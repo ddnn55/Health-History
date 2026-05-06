cf00ae3fa0edc89167d131373b5b74b1c45f1bfe
David Stolarsky <david.stolarsky@gmail.com>
2026-05-06 09:13:00 -0400

    Initial Commit

---
+++ Health History/Health_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_HistoryApp.swift
+//  Health History
+//
+//  Created by David Stolarsky on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Health_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
